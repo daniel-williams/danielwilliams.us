222f13c50e448f1bb61da518df74b4640b347dd3
Daniel James Williams <v-wildan@microsoft.com>
2018-02-16 11:05:51 -0800

    add RxJs lib.

--- src/vendor.tsx
+++ src/vendor.tsx
@@ -4,3 +4,9 @@ import 'react-router';
 import 'mobx';
 import 'mobx-react';
 import 'mobx-react-router';
+
+import 'rxjs';
+// import { Observable } from 'rxjs/Observable';
+// import { Subject } from 'rxjs/Subject';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/map';
