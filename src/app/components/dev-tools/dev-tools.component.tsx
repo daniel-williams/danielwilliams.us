f9279435374b0bfc7fd821daf9af05ed73c9b610
Daniel James Williams <v-wildan@microsoft.com>
2018-03-06 10:40:33 -0800

    Removing explicit EmptyProps and EmptyState interfaces, in favor of using null.

--- src/app/components/dev-tools/dev-tools.component.tsx
+++ src/app/components/dev-tools/dev-tools.component.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 
 import { appConstants } from '../../app.constants';
-import { EmptyProps, EmptyState } from '../../models';
 
 
-export class DevTools extends React.Component<EmptyProps, EmptyState> {
+export class DevTools extends React.Component<null, null> {
   renderDevTool() {
     if (appConstants.isDev) {
       const MobxReactDevTools = require('mobx-react-devtools').default;
--- src/app/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './EmptyProps';
-export * from './EmptyState';
