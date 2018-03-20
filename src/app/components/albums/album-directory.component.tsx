2346408a900802c0814776ab2c09497e37e76e1b
Daniel James Williams <v-wildan@microsoft.com>
2018-03-19 18:43:10 -0700

    Update AlbumDirectory to use InfoCard and InfoCardGrid.

--- src/app/components/albums/album-directory.component.tsx
+++ src/app/components/albums/album-directory.component.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { InfoCard, LayoutManager } from '../shared';
+import { InfoCard, InfoCardGrid } from '../shared';
 import * as styles from './album-directory.component.scss';
 
 
@@ -11,26 +11,32 @@ interface AlbumDirectoryProps {
 
 export const AlbumDirectory = withRouter(class extends React.Component<AlbumDirectoryProps, {}> {
   render() {
-    const names = `${styles.layoutItem} ${styles.clickable}`;
+    const cards = [
+      {route: '/albums/one', comp: Card1},
+      {route: '/albums/two', comp: Card2},
+      {route: '/albums/one', comp: Card3},
+      {route: '/albums/two', comp: Card4},
+    ];
+    const itemStyles = {
+      minWidth: '300px',
+      maxWidth: '500px',
+      height: '100%',
+    };
+    const items = cards.map((x, i) => (
+      <InfoCard
+        key={x.route}
+        onClick={e => this.toRoute(x.route)}
+        style={itemStyles}>
+        {x.comp}
+      </InfoCard>
+    ));
 
     return (
       <div>
         <h1>Albums</h1>
-        <LayoutManager>
-          <div className={names} onClick={() => this.toRoute('/albums/one')}>
-            <InfoCard>
-              <h2>Emerald City</h2>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, possimus! Asperiores, non nemo, dolorum voluptas, a repellat excepturi ratione ipsam sit magni rem placeat ipsum. Accusamus ducimus aspernatur quod tenetur!</p>
-            </InfoCard>
-          </div>
-          <div className={names} onClick={() => this.toRoute('/albums/two')}>
-            <InfoCard>
-              <h2>Microsoft Life</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At sit asperiores, neque saepe quisquam impedit quae consequatur, harum tempora temporibus quo. Iusto, quasi ratione corporis corrupti deserunt officiis nisi aspernatur!</p>
-              <p>Ex unde sunt id facere, enim quia numquam tempore cupiditate reiciendis nisi voluptatibus aliquid vitae, atque hic quas. Consectetur dolorem vero possimus sint modi minus placeat, laudantium eveniet error debitis.</p>
-            </InfoCard>
-          </div>
-        </LayoutManager>
+        <InfoCardGrid>
+          {items}
+        </InfoCardGrid>
       </div>
     );
   }
@@ -39,3 +45,34 @@ export const AlbumDirectory = withRouter(class extends React.Component<AlbumDire
     this.props.history.push(path);
   };
 });
+
+
+const Card1 = (
+  <>
+    <h2>Emerald City</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, possimus! Asperiores, non nemo, dolorum voluptas, a repellat excepturi ratione ipsam sit magni rem placeat ipsum. Accusamus ducimus aspernatur quod tenetur!</p>
+  </>
+);
+
+const Card2 = (
+  <>
+    <h2>Microsoft Life</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At sit asperiores, neque saepe quisquam impedit quae consequatur, harum tempora temporibus quo. Iusto, quasi ratione corporis corrupti deserunt officiis nisi aspernatur!</p>
+    <p>Ex unde sunt id facere, enim quia numquam tempore cupiditate reiciendis nisi voluptatibus aliquid vitae, atque hic quas. Consectetur dolorem vero possimus sint modi minus placeat, laudantium eveniet error debitis.</p>
+  </>
+);
+
+const Card3 = (
+  <>
+    <h2>Emerald City</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, possimus! Asperiores, non nemo, dolorum voluptas, a repellat excepturi ratione ipsam sit magni rem placeat ipsum. Accusamus ducimus aspernatur quod tenetur!</p>
+  </>
+);
+
+const Card4 = (
+  <>
+    <h2>Microsoft Life</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At sit asperiores, neque saepe quisquam impedit quae consequatur, harum tempora temporibus quo. Iusto, quasi ratione corporis corrupti deserunt officiis nisi aspernatur!</p>
+    <p>Ex unde sunt id facere, enim quia numquam tempore cupiditate reiciendis nisi voluptatibus aliquid vitae, atque hic quas. Consectetur dolorem vero possimus sint modi minus placeat, laudantium eveniet error debitis.</p>
+  </>
+);
